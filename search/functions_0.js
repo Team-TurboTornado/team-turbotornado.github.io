98ad66dd5edc05d1f37bc9497e9177bb4d44c6c1
Marc Wagner <mwagner@mail.hs-ulm.de>
2023-01-18 01:00:03 +0100

    update Wed Jan 18 01:00:03 CET 2023

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['addneighbour_0',['AddNeighbour',['../classCore_1_1Services_1_1Animation_1_1TrajectoryPlanning_1_1AStarGrid.html#ae06289339a041dbe5b0b5cfaa90a5f8e',1,'Core::Services::Animation::TrajectoryPlanning::AStarGrid']]],
-  ['animationuicontrol_1',['AnimationUIControl',['../classCore_1_1Services_1_1Animation_1_1AnimationUIControl.html#a2d13395bda83c90ba3be7d27c272f01e',1,'Core::Services::Animation::AnimationUIControl']]],
-  ['autostart_2',['AutoStart',['../classCore_1_1Services_1_1Animation_1_1AnimationUIControl.html#af7647d0745214778eec286343a570ae6',1,'Core::Services::Animation::AnimationUIControl']]]
+  ['animationuicontrol_1',['AnimationUIControl',['../classCore_1_1Services_1_1Animation_1_1AnimationUIControl.html#a2d13395bda83c90ba3be7d27c272f01e',1,'Core::Services::Animation::AnimationUIControl']]]
 ];
